890a0aa32fd2af791f8664dece7363f4cc512bdc
Shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-02-24 20:44:39 +0530

    Day 1:beginner level problem of python

---
+++ test1.py
@@ -0,0 +1,15 @@
+print("hello python")
+print("python is awesome")
+print("iam a good girl \nand viewer is also good girl/boy")#single line comments
+print("currently in\n\"AIML\"")
+a=9
+b="shivani"
+a1=10
+print(a)
+#for complex
+c=(complex(8,2))
+print(b)
+print (c)
+print(a+a1)
+print("the datatype of a",type(a))#to know the  datatype used
+print("the datatype of b",type(b))
