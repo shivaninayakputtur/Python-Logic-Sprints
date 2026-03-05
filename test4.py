5be31cb6be587abcd54874dd7104a901fe126db9
Shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-05 21:03:39 +0530

    problems related to nested loop &for loop

---
+++ test4.py
@@ -0,0 +1,14 @@
+
+for i in range(20):
+     if i%2==0:
+          print(i)
+#printing of even number
+#printing of odd numbers
+for i in range(1,21):
+          if i%2!=0:
+                print(i)
+#sum of numbers from 1-10
+sum=0
+for i in range(1,11):
+     sum=sum+i
+     print(sum)
---
+++ test5.py
@@ -0,0 +1,11 @@
+#star pattern
+for i in range(1,6):
+    for j in range(i):
+        print("*", end = "")
+    print()
+#reverse printing of numbers
+for i in range(10,1,-1):
+    print(i)
+ #multiplication table of 5
+for i in range(0,11):
+    print("5*X", i,"=",5*i)
