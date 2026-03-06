ecc76ff188c0209fdb40bce54efd91eba70dc8c4
Shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-06 21:52:11 +0530

    work of the day=random programs

---
+++ test6.py
@@ -0,0 +1,24 @@
+for i in range(0,11):
+    print("5*X", i,"=",5*i)
+#multiplication table of 5
+print("hello,");print(" python")
+print("python is awesome")
+print("iam a good girl \nand viewer is also good girl/boy")
+print("currently in\n\"AIML\"")
+a=9
+b="shivani"
+a1=10
+print(a)
+#for complex
+c=(complex(8,2))
+print(b)
+print (c)
+print(a+a1)
+print(" a",type(a))#to know the  datatype used
+print("the datatype of b",type(b))
+print("hello\"shivani\"\n\"I LOVE YOU\"")
+print(1,2,3,4,5,sep="~")  
+g=7.9
+print(type(g))
+a="1"
+b=7
