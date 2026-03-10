d303df3bb24c052431957ee6112c3185a3bc55d5
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-10 20:21:23 +0530

    printing numbers with conditions

---
+++ test10.py
@@ -0,0 +1,10 @@
+for i in range(1,31):
+    if(i%3==0 and i%2==0):
+        print("super")
+    elif i%2==0:
+        print("two")
+    elif i%3==0:
+        print("three")
+    else:
+        print(i)
+    
