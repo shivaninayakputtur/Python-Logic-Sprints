8ef6798c8e690d52ba65ca206106b27aedc48b02
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-11 10:22:41 +0530

    rndm program

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
-print("hello python")
+print("hello,");print(" python")
 print("python is awesome")
-print("iam a good girl \nand viewer is also good girl/boy")#single line comments
+print("iam a good girl \nand viewer is also good girl/boy")
 print("currently in\n\"AIML\"")
 a=9
 b="shivani"
@@ -11,5 +11,29 @@ c=(complex(8,2))
 print(b)
 print (c)
 print(a+a1)
-print("the datatype of a",type(a))#to know the  datatype used
+print(" a",type(a))#to know the  datatype used
 print("the datatype of b",type(b))
+print("hello\"shivani\"\n\"I LOVE YOU\"")
+print(1,2,3,4,5,sep="~")  
+g=7.9
+print(type(g))
+#explixit conversion
+a="1"
+b=7
+c=int(a)
+print(c+b)
+#implisit conversion
+a=1 
+b=6.4
+c=a+b
+print(c,type(c))
+s="python is good c is bad"
+print(s.replace("good","bad"))
+#to find length
+name="shivani"
+print(len(name))
+o="shivani%%"
+print(o.rstrip("%"))
+
+
+ 
