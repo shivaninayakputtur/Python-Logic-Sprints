70a339dc4bf93339c6d73376ff8de829a0d88e76
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-15 15:55:17 +0530

    problem of printing star in increasing & decreasing order

---
+++ test18.py
@@ -0,0 +1,10 @@
+#increasing star pattern
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+#decresing star pattern
+for i in range(6,0,-1):
+    for j in range(i):
+       print("*",end="")
+    print()
