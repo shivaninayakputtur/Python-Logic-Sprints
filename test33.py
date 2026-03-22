0e33f8b53c656574c7c81610efcac2eeab85d179
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-22 15:56:14 +0530

    problem related to pangram ,Caesar cipher and capitalization of first word

---
+++ test33.py
@@ -0,0 +1,26 @@
+# alphabet="abcdefghijklmnopqrstuvwxyz"
+# sentence = "the quick brown fox jumps over the lazy dog"
+# # print("a" in sentence)
+# print("z" in sentence)
+# print("q" in sentence)
+# import string
+# sentence="the quick brown fox jumps over the lazy dog"
+# sentence1=sentence.lower()
+# alphabet=string.ascii_lowercase
+# for i in alphabet:
+#     if i in sentence1:
+#         print("found")
+#     else:
+#         print("not found")
+# first=input("enter value:")
+# result=" "
+# for char in first:
+#     result=result+ (chr(ord(char)+3))
+# print(result,end=" ")
+# word=input("enter smthng sexy!!!!!!!! my dear:")
+# word1=word.split()
+# for char in word1:
+#     word2=char.capitalize()
+#     print(word2,end=" ")
+for i in range(10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
+    print("I love Kiran 🥰 will u marry me my dear")
