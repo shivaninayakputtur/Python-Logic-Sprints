7a66e82791811d9f182a6f75cc2291e5fc92bcf2
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-03-26 21:24:32 +0530

    problem to rotate the letter in a given sentence in terms of length

--- test39.py
+++ test39.py
@@ -1,6 +1,14 @@
 word1=input("enter the sentence:")
 word2=input("enter the sentence:")
-if word2 in word1+word1:
+# if word2 in word1+word1:
+#     print("yes rotation")
+# else:
+#     print("not srry ")
+#rotation of letters in word
+#but if length are different
+if len(word1)!=len(word2):
+    print("this is not possible srry")
+elif word2  in word1+word2:
     print("yes rotation")
 else:
-    print("not srry ")
+    print("srry not possible maa")
