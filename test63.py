18f7a2e603876ad8064a2366f6ddabec3d595f60
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-04-13 22:18:28 +0530

    to print all the values inside a 2D array

--- test63.py
+++ test63.py
@@ -3,3 +3,9 @@ num=[[1,2,3],[4,5,6],[7,8,9]]
 print(num[0][1])
 print(num[0][0])
 print(num[2][1])
+# to print numbers
+nums=[[1,2,3],[4,5,6],[7,8,9]]
+for i in nums:
+    for j in i:
+        print(j,end=" ")
+    print()
