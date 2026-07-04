603ba011795903ea0592b6d50f14ba99740cb2ae
shivani Nayak <232409878+shivaninayakputtur@users.noreply.github.com>
2026-07-04 21:39:04 +0530

    Add files via upload

--- all star patten  numbers.py
+++ all star patten  numbers.py
@@ -1,37 +1,37 @@
-# for i in range(1,6):
-#     for j in range(6-i):
-#         print(" ",end="")
-#     for k in range(i):
-#         print("*",end="")
-#     print()
-# #
-# for i in range(6,0,-1):
-#     for j in range(6-i):
-#         print(" ",end="")
-#     for k in range(i):
-#         print("*",end="")
-#     print()
-# #
-# for i in range(1,6):
-#     for j in range(i):
-#         print("*",end="")
-#     print()
-# #
-# for i in range(6,0,-1):
-#     for j in range(i):
-#         print("*",end="")
-#     print()
-# #hollow
-# a=int(input("enter a value"))
-# b=int(input("enter a value"))
-# for i in range(a,b):
-#     for j in range(a,b):
-#         if i==1 or i==4 or j==1 or j==4:
-#             print("*",end=" ")
-#         else:
-#             print("",end=" ")
-#     print()
-# #
+for i in range(1,6):
+    for j in range(6-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
+    print()
+#
+for i in range(6,0,-1):
+    for j in range(6-i):
+        print(" ",end="")
+    for k in range(i):
+        print("*",end="")
+    print()
+#
+for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+#
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*",end="")
+    print()
+#hollow
+a=int(input("enter a value"))
+b=int(input("enter a value"))
+for i in range(a,b):
+    for j in range(a,b):
+        if i==1 or i==4 or j==1 or j==4:
+            print("*",end=" ")
+        else:
+            print("",end=" ")
+    print()
+#
 for i in range(1,6):
     for j in range(5-i):
         print(" ",end=" ")
@@ -41,3 +41,4 @@ for i in range(1,6):
         print("*",end=" ")
     print()
 
+     
